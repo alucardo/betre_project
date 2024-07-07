19c573765232c0b433e20dfc57cc3f4a73ef677a
Przemyslaw Olesinski <przemek.4mm@gmail.com>
2024-07-07 22:31:33 +0200

    Login

--- accounts/views.py
+++ accounts/views.py
@@ -7,6 +7,15 @@ def login(request):
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request, 'You are now logged in')
+            return redirect('dashboard')
+        else:
+            messages.error(request, 'Invalid username or password')
+            return redirect('login')
     else:
         return render(request, 'accounts/login.html')
 
