dae4dedaeea6bec5aaebd62c7f962b6a72e5e8a7
Przemyslaw Olesinski <kontakt@przemyslawolesinski.pl>
2024-07-07 13:31:36 +0200

    Admin customization

--- realtors/admin.py
+++ realtors/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Realtor
+
+class RealtorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'email', 'phone', 'hire_date')
+    list_display_links = ('id', 'name')
+    search_fields = ('name', 'email', 'phone')
+    list_filter = ('hire_date',)
+    list_per_page = 20
+
+admin.site.register(Realtor, RealtorAdmin)
